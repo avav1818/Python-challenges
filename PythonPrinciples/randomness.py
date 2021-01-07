de182d20c02b764ca23615b988b9ff5fce7a3a93
avav1818 <77082071+avav1818@users.noreply.github.com>
2021-01-07 20:41:47 +0000

    Add files via upload

---
+++ PythonPrinciples/randomness.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# https://pythonprinciples.com/challenges/Randomness/
+
+# Random module documentation: https://docs.python.org/3/library/random.html
+from random import randint
+
+def random_number():
+    return randint(1, 101) 
