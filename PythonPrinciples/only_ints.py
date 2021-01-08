fbce78f2d0c5b546f993a36c016f2fb93ade784e
avav1818 <77082071+avav1818@users.noreply.github.com>
2021-01-08 19:16:15 +0000

    Update only_ints.py

--- PythonPrinciples/only_ints.py
+++ PythonPrinciples/only_ints.py
@@ -2,8 +2,8 @@
 
 # https://pythonprinciples.com/challenges/Type-check/ 
 
-def only_ints(v1, v2):
-    if (type (v1) == int ) and (type (v2) == int):
+def only_ints(param1, param2):
+    if (type (param1) == int ) and (type (param2) == int):
         return True
     else:
         return False 
